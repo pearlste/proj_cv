0afa0c70fdb9d7a56af29619d2d9271cc60e0ccb
Larry Pearlstein <pearlstl@lp-research-linux-1.(none)>
2017-03-03 20:16:54 -0500

    Added numerous options to log2csv.py

--- master/script/log2csv.py
+++ master/script/log2csv.py
@@ -5,100 +5,128 @@ import os
 import re
 import subprocess
 import numpy as np
+import argparse
 
+parser = argparse.ArgumentParser()
+parser.add_argument('-s',       action='store_true', help='single experiment mode')
+parser.add_argument('-k',       action='store_true', help='add plot legend'       )
+parser.add_argument('-tstl',    action='store_true', help='include test loss'     )
+parser.add_argument('-tsta',    action='store_true', help='include test accuracy' )
+parser.add_argument('-trnl',    action='store_true', help='include train loss'    )
+parser.add_argument('-trna',    action='store_true', help='include train accuracy')
+parser.add_argument('pattern',     help='regular expression specifying set of experiments in $COLOMBE_ROOT/exp' )
+parser.add_argument('csv_filename_root', default='pf', nargs='?', help='filename root for gnuplot scripts written to $COLOMBE_ROOT/plots/*.gpt')
+args = parser.parse_args()
 
-def Usage():
-    print '''
-Usage: log2csv.py -h
-       log2csv.py --help
-       log2csv.py <pattern> <csv_filename>
+# Usage: log2csv.py -s -tstl -tsta -trnl -trna <pattern> <csv_filename_root>
 
-       <pattern> is any regular expression, used for filtering experiments
-       
-       Creates files:
-          $COLOMBE_ROOT/plots/<csv_filename>_loss.gpt          
-          $COLOMBE_ROOT/plots/<csv_filename>_accuracy.gpt          
-          
-       To run gnuplot command files, run, e.g.:
+mode_sing = 0
+mode_lgnd = 0
+mode_tstl = 0
+mode_tsta = 1
+mode_trnl = 0
+mode_trna = 0
 
-            gnuplot -persist $COLOMBE_ROOT/plots/<csv_filename>_loss.gpt
-'''
-    exit()
-    
-# Get the total number of args passed
-params_total    = len(sys.argv)
+if args.s:
+    mode_sing = 1
+if args.k:
+    mode_lgnd = 1
+if args.tstl:
+    mode_tstl = 1
+if args.tsta:
+    mode_tsta = 1
+if args.trnl:
+    mode_trnl = 1
+if args.trna:
+    mode_trna = 1
 
-if params_total != 3:
-    Usage()
+pattern         = args.pattern
+csv_filename    = args.csv_filename_root
+        
+colombe_root = os.environ['COLOMBE_ROOT']
+the_cwd = colombe_root + '/exp'
+os.chdir(the_cwd)
 
-else:
-    pattern         = sys.argv[1]
-    csv_filename    = sys.argv[2]
+fd_dict = { }
+
+for tst_trn in ( 'tst', 'trn' ):
+    for loss_accu in ( 'loss', 'accu' ):
+
+        type_str = tst_trn + "_" + loss_accu
+        
+        if (tst_trn == 'tst' and loss_accu == 'loss' and mode_tstl) or \
+           (tst_trn == 'tst' and loss_accu == 'accu' and mode_tsta) or \
+           (tst_trn == 'trn' and loss_accu == 'loss' and mode_trnl) or \
+           (tst_trn == 'trn' and loss_accu == 'accu' and mode_trna):
             
-    colombe_root = os.environ['COLOMBE_ROOT']
-    the_cwd = colombe_root + '/exp'
-    os.chdir(the_cwd)
-
-    fd_loss_gpt = open( "../plots/" + csv_filename + "_loss.gpt" , "w" )
-    fd_loss_gpt.write( 'unset key\n' )
-    fd_loss_gpt.write( 'set title "Loss vs. Training Iteration (Log Scale)" font ",14"\n' )
-    fd_loss_gpt.write( 'set logscale y\n' )
-    fd_loss_gpt.write( r'set datafile separator " "' )
-    fd_loss_gpt.write( "\n" )
-
-    fd_accu_gpt = open( "../plots/" + csv_filename + "_accuracy.gpt" , "w" )
-    fd_accu_gpt.write( 'unset key\n' )
-    fd_accu_gpt.write( 'set title "Accuracy vs. Training Iteration" font ",14"\n' )
-    fd_accu_gpt.write( r'set datafile separator " "' )
-    fd_accu_gpt.write( "\n" )
-
-    file_list = os.listdir('.')
-    file_list.sort()
-
-    idx = 0    
-    for the_dir in file_list:
-        m = re.search( pattern, the_dir )
-        if m:
-            # Check for existence of stats files
-            dir_exists = 1
-            try:
-                loss_file = "tst_loss"
-                fd = open( "%s/%s" % (the_dir, loss_file), "r" )
-                fd.close();
-                loss_file = "tst_accu"
-                fd = open( "%s/%s" % (the_dir, loss_file), "r" )
-                fd.close();
-                loss_file = "trn_loss"
-                fd = open( "%s/%s" % (the_dir, loss_file), "r" )
-                fd.close();
-                loss_file = "trn_accu"
-                fd = open( "%s/%s" % (the_dir, loss_file), "r" )
-                fd.close();
-                
-            except (OSError, IOError) as e:
-                dir_exists = 0
-                print "Note: stats file %s/%s does not exist ..." % (the_dir, loss_file)
-                continue
-            
-            if idx == 0:
-                fd_loss_gpt.write( '  plot "%s/exp/%s/tst_loss" using 1:2 with lines title "%s_tst" lc %d\n' % (colombe_root, the_dir, the_dir, idx) )
-                fd_accu_gpt.write( '  plot "%s/exp/%s/tst_accu" using 1:2 with lines title "%s_tst" lc %d\n' % (colombe_root, the_dir, the_dir, idx) )
+            if loss_accu == 'loss':
+                y_title = 'set title "Loss vs. Training Iteration (Log Scale)" font ",14"\n'
             else:
-                fd_loss_gpt.write( 'replot "%s/exp/%s/tst_loss" using 1:2 with lines title "%s_tst" lc %d\n' % (colombe_root, the_dir, the_dir, idx) )
-                fd_accu_gpt.write( 'replot "%s/exp/%s/tst_accu" using 1:2 with lines title "%s_tst" lc %d\n' % (colombe_root, the_dir, the_dir, idx) )
+                y_title = 'set title "Accuracy vs. Training Iteration" font ",14"\n'
+                
+            fd_dict[type_str] = open( "../plots/" + csv_filename + "_" + type_str + ".gpt" , "w" )
+            if not mode_lgnd:
+                fd_dict[type_str].write( 'unset key\n' )
+            fd_dict[type_str].write(y_title)
+            if loss_accu == 'loss':
+                fd_dict[type_str].write( 'set logscale y\n' )
+            fd_dict[type_str].write( r'set datafile separator " "' )
+            fd_dict[type_str].write( "\n" )
+        else:
+            fd_dict[type_str] = None
 
-            idx += 1
+file_list = os.listdir('.')
+file_list.sort()
 
-    fd_loss_gpt.write( 'set terminal postscript noenhanced\n' )
-    fd_loss_gpt.write( 'set output "../plots/%s_loss.ps"\n' % csv_filename )
-    fd_loss_gpt.write( 'replot\n' )
-    fd_loss_gpt.close()
-    
-    fd_accu_gpt.write( 'set terminal postscript noenhanced\n' )
-    fd_accu_gpt.write( 'set output "../plots/%s_accuracy.ps"\n' % csv_filename )
-    fd_accu_gpt.write( 'replot\n' )
-    fd_accu_gpt.close()
-    
-    os.system( "gnuplot -persist  %s/plots/%s_accuracy.gpt" % (colombe_root, csv_filename) )
-    os.system( "gnuplot -persist  %s/plots/%s_loss.gpt" % (colombe_root, csv_filename) )
-    
+idx = 0    
+for the_dir in file_list:
+    m = re.search( pattern, the_dir )
+    if m:
+        # Check for existence of stats files
+        dir_exists = 1
+        try:
+            loss_file = "tst_loss"
+            fd = open( "%s/%s" % (the_dir, loss_file), "r" )
+            fd.close();
+            loss_file = "tst_accu"
+            fd = open( "%s/%s" % (the_dir, loss_file), "r" )
+            fd.close();
+            loss_file = "trn_loss"
+            fd = open( "%s/%s" % (the_dir, loss_file), "r" )
+            fd.close();
+            loss_file = "trn_accu"
+            fd = open( "%s/%s" % (the_dir, loss_file), "r" )
+            fd.close();
+            
+        except (OSError, IOError) as e:
+            dir_exists = 0
+            print "Note: stats file %s/%s does not exist ..." % (the_dir, loss_file)
+            continue
+        
+        if idx == 0:
+            plot_cmd = '  plot'
+        else:
+            plot_cmd = 'replot'
+        
+        for tst_trn in ( 'tst', 'trn' ):
+            for loss_accu in ( 'loss', 'accu' ):
+        
+                type_str = tst_trn + "_" + loss_accu
+                #print( type_str, fd_dict[type_str] )
+                if fd_dict[type_str] is not None:
+                    fd_dict[type_str].write( '%s "%s/exp/%s/%s" using 1:2 with lines title "%s_%s" lc %d\n' % (plot_cmd, colombe_root, the_dir, type_str, the_dir, type_str, idx) )
+
+        idx += 1
+
+for tst_trn in ( 'tst', 'trn' ):
+    for loss_accu in ( 'loss', 'accu' ):
+
+        type_str = tst_trn + "_" + loss_accu
+        if fd_dict[type_str] is not None:
+            fd_dict[type_str].write( 'set terminal postscript noenhanced\n' )
+            fd_dict[type_str].write( 'set output "%s/plots/%s_%s.ps"\n' % (colombe_root, csv_filename, type_str) )
+            fd_dict[type_str].write( 'replot\n' )
+            fd_dict[type_str].close()
+            
+            os.system( "gnuplot -persist  %s" % fd_dict[type_str].name )
+
